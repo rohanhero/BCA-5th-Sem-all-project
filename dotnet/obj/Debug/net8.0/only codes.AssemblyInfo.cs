d91c116831c2e74209842cd9488c9d4fa3bf1add
Rohan <rohanbista24@gmail.com>
2024-07-15 07:25:03 +0545

    relational operator

--- dotnet/obj/Debug/net8.0/only codes.AssemblyInfo.cs
+++ dotnet/obj/Debug/net8.0/only codes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("only codes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd352e02a8be391a592aa5b7968f2839514f148")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+909a1c29c72ea5a9d91ce59b0ed8aa3b4918714e")]
 [assembly: System.Reflection.AssemblyProductAttribute("only codes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("only codes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ dotnet/relationalOperatorOverloadinig.cs
@@ -0,0 +1,61 @@
+using System;
+
+
+class Program
+{
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Program(int hours, int minutes, int seconds)
+    {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public void display()
+    {
+        Console.WriteLine($"Time: {hours}:{minutes}:{seconds}");
+    }
+
+    // constructor overloading
+    public static Program operator +(Program p1, Program p2)
+    {
+        return new Program(p1.hours + p2.hours, p1.minutes + p2.minutes, p1.seconds + p2.seconds);
+    }
+
+    public static bool operator <(Program p1, Program p2)
+    {
+        return p1.hours < p2.hours;
+    }
+
+    public static bool operator >(Program p1, Program p2)
+    {
+        return p1.hours < p2.hours;
+    }
+
+    // constructor overloading
+}
+
+class q9
+{
+    public static void Main(string[] args)
+    {
+        Program p1 = new Program(1, 4, 3);
+        Program p2 = new Program(5, 44, 2);
+
+        Program p3 = p1 + p2;
+
+        p3.display();
+        if (p1 < p2)
+        {
+            Console.WriteLine("It is less hai ta");
+        }
+        else
+        {
+            Console.WriteLine("It is not less hai ta");
+        }
+    }
+}
+
