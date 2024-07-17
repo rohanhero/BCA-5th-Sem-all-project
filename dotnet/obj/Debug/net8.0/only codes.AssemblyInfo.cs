8da6ddae1c233929882a42b1726cebb5bae0b0ad
Aayush <therespawner69@gmail.com>
2024-07-17 06:18:58 +0545

    updates

--- dotnet/obj/Debug/net8.0/only codes.AssemblyInfo.cs
+++ dotnet/obj/Debug/net8.0/only codes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("only codes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d084f0dbb2d5ebdfbccf706df2e661c33c5baa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1661f068a913b4247840843292692dfdd2cfaa11")]
 [assembly: System.Reflection.AssemblyProductAttribute("only codes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("only codes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
