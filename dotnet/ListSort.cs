dc05b12b60453cc1d7a9e33c823bfb24857a7713
Aayush <therespawner69@gmail.com>
2024-07-23 06:07:54 +0545

    Stack in C sharp

---
+++ dotnet/ListSort.cs
@@ -0,0 +1,31 @@
+// using System;
+// using System.Collections.Generic;
+
+
+// class ListinCsharp
+// {
+//     public static void Main(string[] args)
+//     {
+//         List<string> list = new List<string>();
+
+//         list.Add("G");
+//         list.Add("C");
+//         list.Add("B");
+//         list.Add("K");
+//         list.Add("A");
+
+//         foreach (string item in list)
+//         {
+//             Console.WriteLine(item);
+//         }
+
+
+//         list.Sort();
+//         Console.WriteLine("Sorted List: ");
+
+//         foreach(string item in list){
+//             Console.WriteLine(item);
+//         }
+
+//     }
+// }
---
+++ dotnet/ListinCsharp.cs
@@ -0,0 +1,22 @@
+// using System;
+// using System.Collections.Generic;
+
+
+// class ListinCsharp
+// {
+//     public static void Main(string[] args)
+//     {
+//         List<int> list = new List<int>();
+
+//         list.Add(32);
+//         list.Add(2);
+//         list.Add(13);
+//         list.Add(23);
+//         list.Add(22);
+
+//         foreach (int item in list)
+//         {
+//             Console.WriteLine(item);
+//         }
+//     }
+// }
