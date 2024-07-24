e02ddfa2944edbdde4ebe8f94c197268f40fca23
Aayush <therespawner69@gmail.com>
2024-07-24 06:41:23 +0545

    generics dictionary

---
+++ dotnet/QueueinCSharp.cs
@@ -0,0 +1,27 @@
+// using System;
+// using System.Collections.Generic;
+
+
+// class QueueinCSharp
+// {
+//     public static void Main(string[] args)
+//     {
+//         Queue<string> queue1 = new Queue<string>();
+
+//         queue1.Enqueue("G");  //front
+//         queue1.Enqueue("A");
+//         queue1.Enqueue("C"); 
+//         queue1.Enqueue("H");  //rear
+//         queue1.Dequeue();
+
+
+//         // Printing all elements
+//         foreach (string item in queue1)
+//         {
+//             Console.WriteLine(item);
+//         }
+
+//         // Using peek function to show
+//         Console.WriteLine($"This is peek element: {queue1.Peek()} ");
+//     }
+// }
